a7f8dbf3cfdd95774d2c28065d6263fe125b075a
SaeedYasen <saeedyasen41@gmail.com>
2025-10-04 14:09:42 +0300

    Full ToDoApp local  version 2.1  (backend+frontend)  ready

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}", // المسارات حسب مشروعك
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
